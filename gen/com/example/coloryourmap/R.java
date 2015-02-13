b8f974598d238b078a21c8802cfb7a3725c879a8
leeyangwoo <lalunamaki@gmail.com>
2015-02-13 19:01:44 +0900

    Nowin bucketlist activity

--- gen/com/example/coloryourmap/R.java
+++ gen/com/example/coloryourmap/R.java
@@ -22,26 +22,35 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int coloryourmap_logo=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int add=0x7f020000;
+        public static final int coloryourmap_logo=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
         public static final int LogoSplash=0x7f080000;
-        public static final int action_settings=0x7f080001;
+        public static final int action_settings=0x7f080005;
+        public static final int btn_nowin_bucket_accomp=0x7f080003;
+        public static final int menu_add_bucket=0x7f080006;
+        public static final int nowin_bucket_list=0x7f080001;
+        public static final int title_nowin_bucket=0x7f080002;
+        public static final int title_nowin_bucket2=0x7f080004;
     }
     public static final class layout {
         public static final int activity_logo_splash=0x7f030000;
         public static final int activity_main=0x7f030001;
+        public static final int activity_nowin_bucket=0x7f030002;
     }
     public static final class menu {
         public static final int logo_splash=0x7f070000;
         public static final int main=0x7f070001;
+        public static final int nowin_bucket=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
         public static final int title_activity_logo_splash=0x7f050003;
+        public static final int title_activity_nowin_bucket=0x7f050004;
     }
     public static final class style {
         /** 
